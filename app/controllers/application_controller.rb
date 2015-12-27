01d360bb3ffb510fd28b4d9f66ee250bd76240b5
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-27 18:26:50 -0500

    setup root route and message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def whats_up
+    render text: "Hey, What's up?"
+  end
+
 end
