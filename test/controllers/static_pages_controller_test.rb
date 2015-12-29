3131add32b6cdc2ebc22a06662117dfc844b00c6
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-12-29 18:50:25 -0500

    added setup method and instance variable

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Rails App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Rails App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Rails App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about 
     assert_response :success
-    assert_select "title", "About | Rails App"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
