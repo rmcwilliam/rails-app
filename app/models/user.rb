3cf9db016c32967c68cc6d353d1c98424c403b7e
Ryan McWilliam <ryanmcwilliam@gmail.com>
2016-01-12 16:23:05 -0500

    model validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates(:name, presence: true, length: {maximum: 46})
+  validates(:email, presence: true, length: {maximum: 53})
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup 
+    @user = User.new(name: "Ryan McWilliam", email: "rmcwilliam@gmail.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "name should not be really long" do
+    @user.name = "t" * 62
+    assert_not @user.valid?
+  end
+
+  test "email should not be really long" do
+    @user.email = "g" * 57 + "@gmail.com"
+    assert_not @user.valid?
+  end
 end
